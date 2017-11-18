8e277e362dce5282f9c5b8db1381c8803ac85bf9
hanxiao <1350690718@qq.com>
2017-11-18 21:04:39 +0800

    文章管理完成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\ArticleService;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
@@ -9,8 +10,27 @@ class ArticleController extends Controller
     public function __construct(ArticleService $articleService){
         $this->articleService=$articleService;
     }
-    public function addNew()
+    public function addNew(Request $request)
     {
-
+        $rule=[
+            'title'=>'required',
+            'articlecontent'=>'required',
+            'user_id'=>'required'
+        ];
+        $this->validate()
+        $this->articleService->addArticle($request);
     }
+    public function delArticle($article_id)
+    {
+        $this->articleService->delArticle($article_id);
+    }
+    public function showSingleArticle($article_id)
+    {
+        return $this->articleService->selectSingleArticle($article_id);
+    }
+    public function showArticles()
+    {
+        return $this->articleService->selectArticleList();
+    }
+
 }
